4afb22637225ec344cfac8934cc324be1d6adfa0
Johnny <jcgall517@gmail.com>
2025-04-03 14:08:58 -0600

    added probe model citation, and worked on modelling probe

--- README.md
+++ README.md
@@ -15,3 +15,5 @@ https://doi.org/10.1016/j.ultramic.2009.05.012.
 (https://www.sciencedirect.com/science/article/pii/S0304399109001284)
 Abstract: The ptychographical iterative engine (or PIE) is a recently developed phase retrieval algorithm that employs a series of diffraction patterns recorded as a known illumination function is translated to a set of overlapping positions relative to a target sample. The technique has been demonstrated successfully at optical and X-ray wavelengths and has been shown to be robust to detector noise and to converge considerably faster than support-based phase retrieval methods. In this paper, the PIE is extended so that the requirement for an accurate model of the illumination function is removed.
 Keywords: Phase retrieval; Ptychography; Diffractive imaging
+
+Zeping Qin, Zijian Xu, Ruoru Li, Haigang Liu, Shilei Liu, Qingcao Wen, Xing Chen, Xiangzhi Zhang, and Renzhong Tai, "Initial probe function construction in ptychography based on zone-plate optics," Appl. Opt. 62, 3542-3550 (2023) 
--- src/ptycho.py
+++ src/ptycho.py
@@ -1,3 +1,48 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from numpy.fft import fft2, ifft2, fftshift, ifftshift
+from scipy.constants import h, c, e
+from scipy.special import j1
+from skimage.data import camera
+
+ground_truth = camera()
+
+# construct Fresnel initial probe model at focal plane
+def E1(r, E0, wavlen, zp_inner_rad, zp_outer_rad, zp_f):
+    k = 2*np.pi/wavlen
+    th = r/zp_f
+    wave = (-2j*E0/(wavlen*zp_f))*np.exp(1j*k*zp_f*(1 + th**2/2))
+    bessel_term = lambda zp_r: zp_r**2 * j1(k*zp_r*th)/(k*zp_r*th)
+    return wave*(bessel_term(zp_inner_rad) - bessel_term(zp_outer_rad))
+
+# get defocused probe model with angular-spectrum propagation
+def angular_spectrum_propagation(psi, df_z, wavelen, n):
+    q = 1 / wavelen # figure out what momentum transfer actual is
+    Dz = np.exp(-2j*np.pi*df_z/(wavlen*np.sqrt(1 - (q*wavlen)**2)))
+    return ifft2(fft2(psi)*Dz)
+
+# experimental parameters, all length-scales in nm
+E0 = 1 # flux, not sure yet what to put here
+wavlen = h*c*e/12e3 # 12keV
+zp_inner_rad = 25e3 # beamstop radius 25um
+zp_outer_rad = 125e3 # zone-plate outer radius 125um
+zp_f = 60e6 # 60mm zone-plate focal length
+E1_args = (E0, wavlen, zp_inner_rad, zp_outer_rad, zp_f)
+
+# create focused probe 64x64 
+px_size = 10 # 10nm pixel size
+probe_axis = np.linspace(-px_size*32, px_size*32, 64)
+xs, ys = np.meshgrid(probe_axis, probe_axis)
+focused_probe = E1(xs**2 + ys**2, *E1_args)
+
+plt.imshow(np.angle(focused_probe))
+plt.show()
+
+# create de-focused probe
+df_z = 60e3 # 60um defocus
+
+
+# plan: use Fresnel Zone-Plate model probe to simulate data
+# use much simpler probe for initial reconstruction
+# random with circular support slightly larger than zp_outer_rad
 
