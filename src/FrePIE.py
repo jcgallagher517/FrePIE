44bddcde73d9e922b081671e80615628d0476c05
Johnny <jcgall517@gmail.com>
2025-04-09 13:13:45 -0600

    began real work

--- src/FrePIE.py
+++ src/FrePIE.py
@@ -25,6 +25,23 @@ indata = np.cos(template) + 0j
 outdata = call_fft(indata)
 """
 
+lib = ctypes.cdll.LoadLibrary("ePIElibc/ePIE.so")
+ePIE_c = lib.ePIE
+ePIE_c.restype = None
+complex128_2d_ptr = ndpointer(dtype=np.complex128, ndim = 2, flags = "C_CONTIGUOUS")
+ePIE.argtypes = [complex128_2d_ptr, # object
+                 complex128_2d_ptr, # probe
+                 ndpointer(dtype = np.float64, ndim = 3, flags = "C_CONTIGUOUS"), # dps
+                 ndpointer(dtype = np.float64, ndim = 2, flags = "C_CONTIGUOUS"), # scan_pos
+                 ctypes.c_double, # obj step
+                 ctypes.c_double, # prb step
+                 ctypes.c_int, # n_iters
+                 ctypes.c_int, # obj_dx
+                 ctypes.c_int, # obj_dy
+                 ctypes.c_int, # prb_dx
+                 ctypes.c_int, # prb_dy
+                 ctypes.c_int, # number of dps
+                 ndpointer(dtype = np.float64, ndim = 1, flags = "C_CONTIGUOUS")] # error
 
 
 def FrePIE(init_obj, init_prb, dps, scan_pos,
@@ -38,6 +55,19 @@ def FrePIE(init_obj, init_prb, dps, scan_pos,
     outputs:
         rec_object, rec_probe, error_array
     """
-    pass
+    assert (init_prb.shape == dps.shape[1:]), "probe and dps must have same dim"
+
+    obj = np.ascontiguousarray(np.copy(init_obj))
+    prb = np.ascontiguousarray(np.copy(init_prb))
+    c_dps = np.ascontiguousarray(np.copy(dps))
+    c_scan_pos = np.ascontiguousarray(np.copy(scan_pos))
+    error_arr = np.ascontiguousarray(np.empty((n_iters)))
+
+    ePIE(obj, prb, c_dps, c_scan_pos, obj_step, prb_step, n_iters
+         init_obj.shape[0], init_obj.shape[1],
+         init_prb.shape[0], init_prb.shape[1],
+         dps.shape[0], error_arr)
+
+    return obj, prb, error_arr
 
 
--- src/ePIElibc/ePIE.c
+++ src/ePIElibc/ePIE.c
@@ -11,3 +11,17 @@ void compute_fft(size_t size, fftw_complex *input, fftw_complex *output) {
 }
 */
 
+void ePIE(fftw_complex *obj, fftw_complex *prb,
+          double *dps, double *scan_pos,
+          double obj_step, double prb_step, int n_iters,
+          int obj_dx, int obj_dy, int prb_dx, int prb_dy, int n_pats,
+          double *error_arr) {
+
+  int error_per_iter;
+
+  for (int iter = 1; iter <= n_iters; ++iter) {
+
+    
+
+  }
+}
